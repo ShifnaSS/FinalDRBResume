a0a274cb583962f5e8eb8ca81292eb2a08d4e6e1
ShifnaSS <shifnashereef@gmail.com>
2022-01-06 20:13:47 +0530

    commit

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(cors());
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: false}));
 app.use(express.static('./dist/frontend'));
-app.use(express.static('/src/assets/images'));
+app.use(express.static('./dist/frontend/src/assets/images'));
 const storage = multer.diskStorage({
     destination:(req,file,callback)=>{
         callback(null,'./dist/frontend/src/assets/images')
